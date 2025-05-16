97f21eefbc13efb0863343d6ed8b247a6172f364
Alexunder <skosarigin@gmail.com>
2025-05-16 09:05:52 +0300

    remove extra comments

--- task7/main.cpp
+++ task7/main.cpp
@@ -3,14 +3,13 @@
 #include "set.h"
 
 int main(int, char**) {
-  Set<int> a({1, 2, 3});  // Использует SetListImpl
+  Set<int> a({1, 2, 3});
   Set<int> b({1, 4, 9});
   a.add_elem(4);
 
   std::cout << a << std::endl;
-  // ...
   for (int i = 0; i < 20;
-       ++i) {  // При превышении THRESHOLD переключится на SetHashImpl
+       ++i) { 
     a.add_elem(i);
     b.add_elem(i * i);
   }
