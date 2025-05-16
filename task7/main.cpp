837014c46f9e4d56e8fb90879c55deb6da9d2cda
Alexunder <skosarigin@gmail.com>
2025-05-16 09:05:25 +0300

    add example

--- task7/main.cpp
+++ task7/main.cpp
@@ -4,6 +4,7 @@
 
 int main(int, char**) {
   Set<int> a({1, 2, 3});  // Использует SetListImpl
+  Set<int> b({1, 4, 9});
   a.add_elem(4);
 
   std::cout << a << std::endl;
@@ -11,7 +12,11 @@ int main(int, char**) {
   for (int i = 0; i < 20;
        ++i) {  // При превышении THRESHOLD переключится на SetHashImpl
     a.add_elem(i);
+    b.add_elem(i * i);
   }
 
   std::cout << a << std::endl;
+  std::cout << b << std::endl;
+  std::cout << a.get_union(b) << std::endl;
+  std::cout << a.get_intersection(b) << std::endl;
 }
